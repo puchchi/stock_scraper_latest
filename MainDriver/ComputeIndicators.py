22c1bfbd4a4e276f260119dcc4b4f3ff299a6042
puchchi <singhanurag50@gmail.com>
2018-01-03 21:46:58 +0530

    More indicators

--- MainDriver/ComputeIndicators.py
+++ MainDriver/ComputeIndicators.py
@@ -3,7 +3,7 @@ from os import path
 sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 
 import pandas as pd
-from Indicators import AccumulationNDistributionLine, OnBalanceVolume, AverageDirectionalIndex, MACD
+from Indicators import AccumulationNDistributionLine, OnBalanceVolume, AverageDirectionalIndex, MACD, RSI
 
 class kComputeIndicators:
     def __init__(self, filename):
@@ -17,28 +17,33 @@ class kComputeIndicators:
         dataframe = self.df
 
         # Calculating ADL
-        #adl = AccumulationNDistributionLine.kAccumulationNDistributionLine()
-        #adlSeries = adl.calculate(dataframe)
-        #adlDataframe = pd.DataFrame(adlSeries, columns=["ADL"])
-        #dataframe.merge(adlDataframe, left_index=True, right_index=True)
+        adl = AccumulationNDistributionLine.kAccumulationNDistributionLine()
+        adlSeries = adl.calculate(dataframe)
+        adlDataframe = pd.DataFrame(adlSeries, columns=["ADL"])
 
         # Calculating OBV
-        #obv = OnBalanceVolume.kOnBalanceVolume()
-        #obvSeries = obv.calculate(dataframe)
-        #obvDataframe = pd.DataFrame(obvSeries, columns=["OBV"])
+        obv = OnBalanceVolume.kOnBalanceVolume()
+        obvSeries = obv.calculate(dataframe)
+        obvDataframe = pd.DataFrame(obvSeries, columns=["OBV"])
 
         # Calculating ADX
-        #adx = AverageDirectionalIndex.kAverageDirectionalIndex();
-        #adxDataframe = adx.calculate(dataframe)
+        adx = AverageDirectionalIndex.kAverageDirectionalIndex()
+        adxDataframe = adx.calculate(dataframe)
 
         # Calculating MACD
-        macd = MACD.kMACD();
+        macd = MACD.kMACD()
         macdDataframe = macd.Calculate(dataframe)
 
-        #dataframe = dataframe.merge(adlDataframe, left_index=True, right_index=True)
-        #dataframe = dataframe.merge(obvDataframe, left_index=True, right_index=True)
-        #dataframe = dataframe.merge(adxDataframe, left_index=True, right_index=True)
+        # Calculating RSI
+        rsi = RSI.kRSI()
+        rsiSeries = rsi.Calculate(dataframe)
+        rsiDataframe = pd.DataFrame(rsiSeries, columns=["RSI"])
+
+        dataframe = dataframe.merge(adlDataframe, left_index=True, right_index=True)
+        dataframe = dataframe.merge(obvDataframe, left_index=True, right_index=True)
+        dataframe = dataframe.merge(adxDataframe, left_index=True, right_index=True)
         dataframe = dataframe.merge(macdDataframe, left_index=True, right_index=True)
+        dataframe = dataframe.merge(rsiDataframe, left_index=True, right_index=True)
 
         return dataframe
 
